f6db133ec1f5b62814f4d43a63931a7cc3671706
Deepak <deepak2603dk@gmail.com>
2023-10-29 16:30:38 +0530

    second commit

--- src/Component/WeatherApp.jsx
+++ src/Component/WeatherApp.jsx
@@ -5,6 +5,7 @@ import { useRef, useState } from "react"
 function WeatherApp() {
     let [city ,setCity] = useState("not Found🤖")
     let [temp,setTemp] = useState(null)
+    let [desc,setDesc] = useState(null)
     
     
     const cityref = useRef("Enter city please")
@@ -12,10 +13,17 @@ function WeatherApp() {
 
         const fetchData= async()=>{
             let resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d2620260bc65f1da55df07eafb0b41b`);
-            let weahterData = await resp.json();
-            console.log('>>>>>>>>>>>', weahterData)
-            temp = (weahterData.main.temp-271.15).toFixed(1) 
-            setTemp(temp)
+            let weatherData = await resp.json();
+            console.log('>>>>>>>>>>>', weatherData)
+            let temperature = (weatherData.main.temp-271.15).toFixed(1) ;
+            let desc = weatherData.weather[0].description
+            console.log('>>>>>>>>>>>', desc)
+
+            console.log('>>>>>>>>>>>', temperature)
+            console.log('>>>>>>>>>>>', desc)
+            setTemp(temperature)
+         
+          
 
         }
         fetchData();
@@ -31,7 +39,7 @@ function WeatherApp() {
 
   return (
     <>
-    <div className="cont h-screen w-screen bg-slate-700 flex justify-center items-center">
+    <div className="cont h-screen w-screen bg-slate-700  flex justify-center items-center"  > 
           <div className="card h-[30rem] w-[20rem] bg-slate-400 rounded-lg flex  items-center flex-col gap-6 shadow-lg">
             {/* input  */}
             <div className="input w-screnn mt-8 ">
@@ -46,9 +54,10 @@ function WeatherApp() {
                 {/* location  */}
              <div className="location"><h1 className="font-semibold text-3xl">{city}</h1></div>
              {/* temp  */}
+        
              <div className="temp font-thin text-5xl"><h1>{temp}</h1></div>
             {/* extra info */}
-            <div className="extra"><h2>extra</h2></div>
+            <div className="extra"><h2>{desc}</h2></div>
             </div>
           </div>
     </div>
